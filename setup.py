1a406125c6b1f5a3661d3cde51ad0a597d788de4
saullocastro <saullogiovani@gmail.com>
2019-09-12 09:41:54 +0200

    BLD: improving build

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import re
 import ast
-from setuptools import setup, Extension
+from setuptools import setup
 import numpy
+from distutils.extension import Extension
+from Cython.Build import cythonize
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
@@ -30,34 +32,29 @@ with open('cubature/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-try:
-    from Cython.Distutils import build_ext
-    ext_modules = [
-        Extension('cubature._cubature',
-            sources = [
-                'cubature/_cubature.pyx',
-                'cubature/cpackage/hcubature.c',
-                'cubature/cpackage/pcubature.c',
-                'cubature/get_ptr.c'],
-            include_dirs = [numpy.get_include()]),
-        Extension('cubature._test_integrands',
-            sources = ['cubature/_test_integrands.pyx'],
-            include_dirs = [numpy.get_include()]),
-    ]
-    cmdclass = {'build_ext': build_ext}
-except ImportError:
-    ext_modules = [
-            Extension('cubature._cubature', ['cubature/_cubature.c']),
-            Extension('cubature._test_integrands', ['cubature/_test_integrands.c'])
-            ]
-    cmdclass = {}
+extensions = [
+    Extension('cubature._cubature',
+        sources = [
+            'cubature/cpackage/hcubature.c',
+            'cubature/cpackage/pcubature.c',
+            'cubature/get_ptr.c',
+            'cubature/_cubature.pyx',
+            ],
+        include_dirs = [numpy.get_include()],
+        language='c',
+        ),
+    Extension('cubature._test_integrands',
+        sources = ['cubature/_test_integrands.pyx'],
+        include_dirs = [numpy.get_include()],
+        language='c',
+        ),
+]
 
 setup(
     name = 'cubature',
     version = version,
-    cmdclass = cmdclass,
     packages = ['cubature'],
-    ext_modules = ext_modules,
+    ext_modules = cythonize(extensions),
     include_dirs = [numpy.get_include()],
     author = 'Saullo G. P. Castro and Anton Loukianov',
     author_email = 'saullogiovani@gmail.com',
