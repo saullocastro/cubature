0f2dd58c2d66c271993fb911c7d9c458114382c0
saullocastro <saullogiovani@gmail.com>
2019-09-13 08:31:53 +0200

    TST: fix CI

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ Intended Audience :: Education
 Topic :: Scientific/Engineering :: Mathematics
 License :: OSI Approved :: BSD License
 Operating System :: Microsoft :: Windows
+Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
