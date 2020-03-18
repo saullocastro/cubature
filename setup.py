5fbad3a77d23ed126009befb112bf5ad0ec406d3
saullocastro <saullogiovani@gmail.com>
2020-03-18 17:53:41 +0100

    BLD: fixing compatibility with MAC OS

--- setup.py
+++ setup.py
@@ -35,13 +35,6 @@ with open('cubature/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-if os.name == 'nt':
-    compile_args = ['/openmp', '/O2']
-    link_args = []
-else:
-    compile_args = ['-fopenmp', '-O2', '-static', '-static-libgcc', '-static-libstdc++']
-    link_args = ['-fopenmp', '-static-libgcc', '-static-libstdc++']
-
 extensions = [
     Extension('cubature._cubature',
         sources = [
@@ -51,15 +44,11 @@ extensions = [
             'cubature/_cubature.pyx',
             ],
         include_dirs = [numpy.get_include()],
-        extra_compile_args=compile_args,
-        extra_link_args=link_args,
         language='c',
         ),
     Extension('cubature._test_integrands',
         sources = ['cubature/_test_integrands.pyx'],
         include_dirs = [numpy.get_include()],
-        extra_compile_args=compile_args,
-        extra_link_args=link_args,
         language='c',
         ),
 ]
