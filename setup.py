b714f4e718c0da749af686c9379579a87b452e89
Saullo G P Castro <saullogiovani@gmail.com>
2022-07-21 08:45:20 +0200

    MAINT: dropping Python 3.6 support

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ Intended Audience :: Developers
 Intended Audience :: Education
 Topic :: Scientific/Engineering :: Mathematics
 License :: OSI Approved :: BSD License
-Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
